f3cab53a0cd1f427259f2913f211134c00970ced
Destrayon <39275283+Destrayon@users.noreply.github.com>
2019-08-24 09:35:42 -0700

    Some changes
    .

--- Bot.py
+++ Bot.py
@@ -27,6 +27,9 @@ def formatter(transaction):
     return f'{datetime.datetime.now()} - A new transaction was found:\n{amount}, paid with {type}.\n\n'
 
 
+# This takes a string, in this case the formatted string with all of the new transactions found, and prepares the
+# string with necessary info before sending the string through email. The email and password variables will need
+# to be changed if the sending email account is different.
 def email(str):
     email = os.environ.get("TEST_EMAIL")
     password = os.environ.get("TEST_PASS")
@@ -41,12 +44,14 @@ def email(str):
         server.sendmail(email, email, email_message)
 
 
+# This will return a request made to the necessary api. If the request fails, it will call itself to try to make the
+# request again.
 def initial_requesting():
     try:
         return requests.post('https://api.omniexplorer.info/v1/address/addr/details/', headers=headers, data=data).json()['transactions'][0]
     except:
         print("Error, trying again.")
-
+        time.sleep(20)
         return initial_requesting()
 
 
@@ -64,6 +69,8 @@ accepted_currencies = {
     "Omni Token"
 }
 
+minimum_transaction_amount = 10000.0
+
 initial_request = initial_requesting()
 
 initial_transaction = initial_request['txid']
@@ -78,15 +85,23 @@ while True:
         response_json = response.json()
         transaction = response_json['transactions'][0]
         transaction_txid = transaction['txid']
+        # This just checks to see if the most recent transaction is the same as the initial transaction, if they
+        # aren't, then the code knows that there are potentially new transactions available.
         if transaction_txid != initial_transaction:
             num = find_transaction_amount(response_json)
             str = ''
+            # The for loop will iterate through the new transactions found and prepare them all into a string.
+            # This will leave out transactions that do not meet requirements.
             for index in range(0, num):
                 new_transaction = response_json['transactions'][index]
-                if new_transaction['propertyname'] in accepted_currencies:
+                if new_transaction['propertyname'] in accepted_currencies and float(new_transaction['amount']) >= minimum_transaction_amount:
                     transaction = formatter(new_transaction)
                     str = f'{str}{transaction}'
 
+            # If there is at least one transaction that meets requirements, the str variable should not be blank,
+            # which means that there is a new transaction available to email out. If the str is blank, then
+            # there aren't any new transactions we care about, so the program will let us know there are
+            # no new transactions available, hence no email will be sent out.
             if str != '':
                 email(str)
                 print("Email has been sent!")
